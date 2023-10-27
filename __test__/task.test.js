9535187c13c9b0c11ffd7522a5f57767b2273024
izima01 <kingsleyizima@gmail.com>
2023-10-27 14:43:14 +0100

    made some changes to the global variables

--- __test__/task.test.js
+++ __test__/task.test.js
@@ -1,9 +1,5 @@
 const axios = require('axios');
 
-beforeAll(() => {
-  global.testToken = process.env.testToken;
-});
-
 test('Test for adding items endpoint', async() => {
     try {
         const res = await axios.post('http://localhost:4000/v1/admin/shop/add-item', {
@@ -13,15 +9,15 @@ test('Test for adding items endpoint', async() => {
             isInStock: true
         }, {
             headers: {
-                Authorization: `Bearer ${global.testToken}`
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkdhYmUiLCJ1c2VySWQiOiI2NTNhYzBkNTBiMWQ5ODU4NGY0ZThhMGYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTgzNDk0NjV9.LXl0hqOoLkr6R8hw9FvaZsp5CUTIhyO5btpF_0jPcTA'
             }
         });
+
+        global.productId = res.data.newItem._id;
         
         expect(res.status).toBe(201);
         expect(res.data).toBe('User created successfully');
-    } catch (err) {
-        // console.log(err);
-    }
+    } catch (err) {}
 }, 10000);
 
 
@@ -29,7 +25,7 @@ test('Test for getting all items endpoint', async() => {
     try {
         const res = await axios.get('http://localhost:4000/v1/shop/list-all', {
             headers: {
-                Authorization: `Bearer ${global.testToken}`
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkdhYmUiLCJ1c2VySWQiOiI2NTNhYzBkNTBiMWQ5ODU4NGY0ZThhMGYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTgzNDk0NjV9.LXl0hqOoLkr6R8hw9FvaZsp5CUTIhyO5btpF_0jPcTA'
             }
         });
 
@@ -38,14 +34,14 @@ test('Test for getting all items endpoint', async() => {
     } catch (err) {}
 });
 
-// test('Test for deleting items', async () => {
-//     try {
-//         const res = await axios.get(`http://localhost:4000/v1/admin/shop/delete-item/:id`,  {
-//             headers: {
-//                 Authorization: `Bearer ${global.testToken}`
-//             }
-//         });
+test('Test for deleting items', async () => {
+    try {
+        const res = await axios.get(`http://localhost:4000/v1/admin/shop/delete-item/${global.productId}`, {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkdhYmUiLCJ1c2VySWQiOiI2NTNhYzBkNTBiMWQ5ODU4NGY0ZThhMGYiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTgzNDk0NjV9.LXl0hqOoLkr6R8hw9FvaZsp5CUTIhyO5btpF_0jPcTA'
+            }
+        });
 
-//         expect(res.data.message).toBe("Deleted successfully");
-//     } catch (err) {}
-// });
+        expect(res.data.message).toBe("Deleted successfully");
+    } catch (err) {}
+});
